cc99258b2b9b35c19c59a538aa8a80392e101586
jck000 <jck000@gmail.com>
2018-04-02 15:27:54 +0000

    Added tests for pre-installed version. Better information during installation. Added symlink to executable. Changed chmod

--- install.sh
+++ install.sh
@@ -1,9 +1,35 @@
+#!/bin/sh
 
-cp sitectl.sh /usr/local/bin
-cp sitectl.cfg /etc
+#
+# Author: Jack Bilemjian 
+# https://github.com/jck000/sitectl.git
+#
 
-chmod ugo+rwx /usr/local/bin/sitectl.sh
-chmod go-w /usr/local/bin/sitectl.sh
+clear 
 
-chmod ugo+r /etc/sitectl.cfg
-chmod ugo-wx /etc/sitectl.cfg
+# Copy script to the correct location, create alias and set permissions
+if [ ! -e /usr/local/bin/sitectl.sh ] ; then
+  cp ./sitectl.sh /usr/local/bin
+  ln -s /usr/local/bin/sitectl.sh /usr/local/bin/sitectl
+  chmod 755 /usr/local/bin/sitectl.sh
+else
+  echo -e "Script is already installed\n"
+  exists=1
+fi
+
+# Copy configuration file to the correct location and set permissions
+if [ ! -e /etc/sitectl.cfg ] ; then 
+  cp ./sitectl.cfg /etc
+  chmod 755 /etc/sitectl.cfg
+else
+  echo -e "Configuration file exists\n\n"
+  exists=1
+fi
+
+if [ -n "$exists" ] ; then
+  echo -e "Script or configuration file already exists.  Please remove them to reinstall\n\n"
+fi
+
+ls -l /usr/local/bin/sitectl* /etc/sitectl.cfg
+
+echo -e "Completed\n\n"
